2622649b3e837f785d9aa54f6dcf85316931f75e
n11x <luciano.meniconi.r@gmail.com>
2021-08-11 03:29:25 -0100

    Init

---
+++ src/app.py
@@ -0,0 +1,18 @@
+import os
+
+# FLask
+from flask import Flask
+
+app_name = str(os.environ.get('FLASK_APP', __name__))
+app = Flask(app_name)
+
+
+@app.route('/')
+def index():
+    return 'Hello world from the Alpine Linux!'
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8080))
+    debug = bool(os.environ.get('DEBUG', False))
+    app.run(host='0.0.0.0', port=port, debug=debug)
