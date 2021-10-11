c5a7a11a974434a6fa72ce05cc39c9d791856dd1
lmeniconi <luciano.meniconi.r@gmail.com>
2021-10-11 02:26:59 -0300

    port changed to 3000

--- src/app.py
+++ src/app.py
@@ -13,6 +13,6 @@ def index():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 3000))
     debug = bool(os.environ.get('DEBUG', False))
     app.run(host='0.0.0.0', port=port, debug=debug)
